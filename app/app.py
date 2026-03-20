e7d5d76f83622f1c17ad5d4dc3831802a6fc22c4
Fabio Shindy Kuamoto <fskmoto@gmail.com>
2026-03-20 12:35:26 -0300

    Correção do Dockerfile e ajustes na API Flask

--- app/app.py
+++ app/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+app.json.ensure_ascii = False
 
 @app.route("/")
 def home():
-    return {"message": "API DevOps rodando 🚀"}
+    return jsonify(message="API DevOps rodando 🚀")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
